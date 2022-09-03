da8821ec873c6c08ed2260582eaa76d99b767b09
hugosenari <hugosenari@gmail.com>
2022-09-03 19:18:01 -0300

    chore: re index ski

---
+++ source/examples/read_file/main.nim
@@ -0,0 +1,14 @@
+import ski
+from streams import newFileStream, lines
+import json
+
+# Define combinators from JSON file
+let combinators = readFile("combinators.json").parseJson.to(seq[Combinator])
+
+# Read and calculate from text file
+let strm = newFileStream("in.txt")
+for line in strm.lines:
+  echo line
+  for ret in line.calculateIterator(combinators):
+    echo "  -> " & ret
+strm.close
